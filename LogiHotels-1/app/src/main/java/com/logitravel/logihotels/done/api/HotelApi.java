f3cf6f5289ee5e9da6930cad69875827177cdfc9
Manuel <manuel@yesweapp.eu>
2015-07-21 13:00:18 +0200

    ...

--- LogiHotels-1/app/src/main/java/com/logitravel/logihotels/done/api/HotelApi.java
+++ LogiHotels-1/app/src/main/java/com/logitravel/logihotels/done/api/HotelApi.java
@@ -1,11 +1,4 @@
 package com.logitravel.logihotels.done.api;
 
-import com.logitravel.logihotels.done.model.Hotel;
-
-import java.util.List;
-
-import retrofit.http.GET;
-
 public interface HotelApi {
-    @GET("api/hotel/{city}") List<Hotel> getHotels(int city);
 }
